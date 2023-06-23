2f80fa095687a4500918608cf7b969f7e0763797
Raju012345 <rreddy012345@gmail.com>
2023-06-22 22:27:54 -0500

    practice.

---
+++ java/src/main/DataTypes.java
@@ -0,0 +1,72 @@
+package main;
+
+public class DataTypes {
+    public static void main(String...args){
+        //byte b1 = (byte)128;
+        //System.out.println(b1);
+        //byte b2 = (byte)(b1*3);
+        //System.out.println(b2);
+
+        //int i = 0X20;
+        //System.out.println(i);
+
+        //byte b3 = (byte)300;
+        //System.out.println(b3);
+
+        //int i = 10;
+
+        //int j = i++ + ++i;
+
+        //System.out.println(i);
+
+        //System.out.println(j);
+
+        //int i = 10;
+
+        //int j = ++i;
+
+        //System.out.println(i);
+
+        //System.out.println(j);
+
+        //int i = 10;
+
+        //int j = i++ + i++ + --i + ++i + ++i;
+
+        //System.out.println(i);
+
+        //System.out.println(j);
+
+        //int i = 10;
+
+        //int j= 7;
+
+        //double db1 = i/j;
+
+        //double db1 = (double)i/j;
+
+        //double db1 = i/(double)j;
+
+        //int i = 10;
+
+        //int j = 7;
+
+        //int m = -10;
+
+        //System.out.println(i + j);
+
+
+        int i = 10;
+
+        int j = i++;
+
+        System.out.println(i);
+
+        System.out.println(j);
+
+        System.out.println(18*3/3/4	);
+
+
+
+    }
+}
