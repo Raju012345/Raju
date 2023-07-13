5bd79d5bc5b12430a77952f1b5e8a4e102a092f0
Raju012345 <rreddy012345@gmail.com>
2023-07-12 19:20:01 -0500

    assignment

--- java/src/main/StringExample.java
+++ java/src/main/StringExample.java
@@ -8,3 +8,8 @@ public class StringExample {
 
     }
 }
+
+
+// String is immutable in java because String can used to store as key in map collection. String buffer is mutable.
+
+// The most important reason of a String being made immutable in java is Security consideration and next would be Caching.
